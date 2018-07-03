3d3029dfa9e6b0fa1693c193f663f931568be8b0
slab <slab@dataplane.switch-demo.psi-pg0.wisc.cloudlab.us>
2018-07-03 15:05:45 -0500

    updating snort container

---
+++ docker_containers/snort_direct_block_v2/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+    grep -q '^1$' "/sys/class/net/eth0/carrier" &&
+	grep -q '^1$' "/sys/class/net/eth1/carrier" &&
+	break
+
+    sleep 1
+
+done
+
+ethtool -K eth0 gro off
+ethtool -K eth0 lro off
+ethtool -K eth1 gro off
+ethtool -K eth1 lro off
+
+if [[ ! -s /etc/snort/snort.conf ]]; then
+    mv /etc/snort/snort.conf.default /etc/snort/snort.conf
+fi
+
+if [[ ! -s /etc/snort/rules/local.rules ]]; then
+    mv /etc/snort/rules/local.rules.default /etc/snort/rules/local.rules    
+fi    
+
+exec /usr/local/bin/snort "$@"
