2680097c7ed5fb19c5c317e8ac1bd6c6e0d0b904
slab <slab@node-0.ovs-remote.psi-pg0.wisc.cloudlab.us>
2018-07-04 20:37:59 -0500

    updating ovs tools

--- ovs_remote/install_ovs.sh
+++ ovs_remote/install_ovs.sh
@@ -1,1 +1,16 @@
-sudo apt-get install -yqq openvswitch-switch
+#!/bin/bash
+
+install_ovs() {
+    ##sudo apt-get install -yqq openvswitch-switch
+    cd ~
+    git clone https://github.com/slab14/ovs.git
+    cd ovs
+    git checkout slab
+    ./boot.sh
+    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc
+    make
+    sudo make install
+}
+
+install_ovs
+
---
+++ ovs_remote/start_ovs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ovs_start() {
+    sudo /usr/share/openvswitch/scripts/ovs-ctl start
+}
+
+ovs_start
