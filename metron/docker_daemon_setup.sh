671d07cfe0d56fa3add990a4bc255d2081076c36
slab14 <30778419+slab14@users.noreply.github.com>
2018-07-20 15:44:17 -0400

    updates to metron tools

--- metron/docker_daemon_setup.sh
+++ metron/docker_daemon_setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# make docker able to run without sudo preface
 sudo usermod -a -G docker $USER
+
+# save docker images outside of / filesystem
+## TODO: not working
+sudo sed -i 's/\-H fd:\/\//\-H fd:\/\/ -g \/mnt/g' /lib/systemd/system/docker.service
+#echo 'DOCKER_OPTS="-g /mnt"' | sudo tee -a /etc/default/docker
+#sudo service docker restart
+
--- metron/ubuntu_vm_actions.sh
+++ metron/ubuntu_vm_actions.sh
@@ -59,3 +59,24 @@ ${METRON_HOME}/bin/zk_load_configs.sh -i ${METRON_HOME}/config/zookeeper -m PUSH
 
 ## Start new parser
 ${METRON_HOME}/bin/start_parser_topology.sh -k $BROKERLIST -z $ZOOKEEPER -s squid
+
+
+## currently implementation only has 6 supervisors and all are being used
+## can kill an existing topology (sensor)
+storm kill bro
+
+## Install NiFi
+cd /usr/lib
+sudo wget  http://public-repo-1.hortonworks.com/HDF/centos6/1.x/updates/1.2.0.0/HDF-1.2.0.0-91.tar.gz
+sudo tar -zxvf HDF-1.2.0.0-91.tar.gz
+cd HDF-1.2.0.0/nifi
+sudo sed -i 's/nifi.web.http.port=8080/nifi.web.http.port=8089/g' conf/nifi.properties
+sudo bin/nifi.sh install nifi
+sudo service nifi start
+
+## Check /etc/hosts for node1 node1
+sudo sed -i '/node1\tnode1/d' /etc/hosts
+
+## make elastic search accessible from remote
+echo "network.bind_host: 0" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo service elasticsearch restart
