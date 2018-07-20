3e754283a42aebaae00676ad6e0f3eb68622f9f5
slab <slab@dataplane.switch-m.psi-pg0.wisc.cloudlab.us>
2018-07-19 21:12:54 -0500

    tool for making VM accessible remotely

---
+++ metron/vm_accessible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Find VM name
+VM=VBoxManage list runningvms | awk -F '"' '{ print $2 }'
+
+## Port Forwarding (8080 Ambari, 5000 Kibana, 8744 Storm, 9200 Elastisearch, 8089 NiFi)
+VBoxManage controlvm $VM natpf1 ,tcp,,8080,,8080
+VBoxManage controlvm $VM natpf1 ,tcp,,5000,,5000
+VBoxManage controlvm $VM natpf1 ,tcp,,8744,,8744
+VBoxManage controlvm $VM natpf1 ,tcp,,9200,,9200
+VBoxManage controlvm $VM natpf1 ,tcp,,8089,,8089
+
+## TODO could make port forwarding specific to ip addresses
