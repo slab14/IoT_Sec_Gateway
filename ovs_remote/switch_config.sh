f584c524219236b31b6dee19e5b86c578e8c3080
slab <slab@dataplane.met-demo.psi-pg0.wisc.cloudlab.us>
2018-08-07 13:41:36 -0500

    updating helper script for configuring switches to listen to controller

--- ovs_remote/switch_config.sh
+++ ovs_remote/switch_config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+BRIDGE=$1
+
+if [ -z $BRIDGE ]; then
+    BRIDGE=br0
+fi
+
 # Setting to make vswitch (br0) listen for commands from controller
-sudo ovs-vsctl set-controller br0 tcp:127.0.0.1:6633 ptcp:6634
-sudo ovs-vsctl set bridge br0 protocol=OpenFlow13
-sudo ovs-vsctl set-fail-mode br0 secure
+sudo ovs-vsctl set-controller $BRIDGE tcp:127.0.0.1:6633 ptcp:6634
+sudo ovs-vsctl set bridge $BRIDGE protocol=OpenFlow13
+sudo ovs-vsctl set-fail-mode $BRIDGE secure
