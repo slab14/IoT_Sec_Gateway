c43ca6411f4df492d265cd247c8b876cf21ada25
slab <slab@dataplane.ipsec.psi-pg0.wisc.cloudlab.us>
2019-01-24 22:55:28 -0600

    adding tool to start up openvpn container. Requires input from stdin

---
+++ helper_functions/start_openvpn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+IMGNAME=$1
+CONTIP=$2
+DEVIP=$3
+CLIENTNAME=$4
+CONTNAME=$5
+BRIDGE=$6
+
+cd ~
+mkdir -p vpn
+# Generate the openvpn config file
+sudo docker run -v $PWD/vpn:/etc/openvpn --rm $IMGNAME ovpn_genconfig -u udp://$CONTIP -N -d -p "route 192.1.0.0 255.255.0.0"
+
+# Generate cryto stuff
+sudo docker run -v $PWD/vpn:/etc/openvpn --rm -it $IMGNAME ovpn_initpki
+sudo docker run -v $PWD/vpn:/etc/openvpn -it --rm $IMGNAME easyrsa build-client-full $CLIENTNAME nopass
+sudo docker run -v $PWD/vpn:/etc/openvpn --rm $IMGNAME ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+
+# Start VPN container
+sudo docker run -v $PWD/vpn:/etc/openvpn --rm -d -p 1194:1194/udp --cap-add=NET_ADMIN --network=none --name=$CONTNAME $IMGNAME
+
+# Add interfaces
+sudo ovs-docker add-port $BRIDGE eth0 $CONTNAME --ipaddress=$CONTIP/16
+sudo ovs-docker add-port $BRIDGE eth1 $CONTNAME --ipaddress=$DEVIP/16
+
+sudo docker exec $CONTNAME /sbin/iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
