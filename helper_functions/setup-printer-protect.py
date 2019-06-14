53cf20841bd8fd867b57d7e101cc4ff1dd3e043b
slab <slab@dataplane.demo.psi-pg0.wisc.cloudlab.us>
2019-06-14 10:03:47 -0500

    adding ability to instantiate multiple security chains

--- helper_functions/setup-printer-protect.py
+++ helper_functions/setup-printer-protect.py
@@ -13,6 +13,9 @@ NODE_0='192.1.1.2'
 NODE_1='10.1.1.2'
 BRIDGE='br0'
 
+VPN_CLIENT_SIDE='192.1.2.2'
+VPN_PRINTER_SIDE='10.1.2.2'
+
 ## mbox chain:
 # controlPC -> antidos  -> VPN server -> IPS  -> 3dPrinter
 # 3dPrinter -> antidos   -> IPS   -> VPN server -> controlPC
@@ -24,7 +27,14 @@ EGRESS_CHAIN=['antiDoS:eth1', 'antiDoS:eth0', 'IPS:eth1', 'IPS:eth0', 'VPNserver
 
 
 def get_names(number):
-    nameList = ['test{}'.format(i) for i in range(number)]
+    #nameList = ['test{}'.format(i) for i in range(number)]
+    nameList=[]
+    for i in range(number):
+        tempArray=[]
+        for name,image in CONTAINER_NAME_and_IMAGE_LIST:
+            newName=name+str(i)
+            tempArray.append((newName,image))
+        nameList.append(tempArray)
     return nameList
 
 def get_ip_range(base_ip, num):
@@ -36,34 +46,61 @@ def get_ip_range(base_ip, num):
     ips = [base_ip + i for i in range(num)]
     return ips
 
+def add_chain_number(chain, number):
+    newChain=[]
+    for item in chain:
+        vals=item.split(':')
+        newItem=vals[0]+str(number)+":"+vals[1]
+        newChain.append(newItem)
+    return newChain
+
 def main():
     parser=argparse.ArgumentParser(description='Connect container to vswitch')
     parser.add_argument('--container', '-C', required=False, type=str)
     parser.add_argument('--instances', '-n', required=False, type=int)
     args=parser.parse_args()
-    '''
-    name_list = []
-    client_ips = []
-    server_ips = []
-    name_list = get_names(args.instances)
-    client_ips = get_ip_range(NODE_0, args.instances)
-    server_ips = get_ip_range(NODE_1, args.instances)
-    '''
-    #for i in range(0, len(name_list)):
-    for name,image in CONTAINER_NAME_and_IMAGE_LIST:
-        if image=='openvpn':
-            start_container(image, name, True, ' -v /users/slab/vpn:/etc/openvpn -p 1194:1194/udp')
-            attach_container_wIP(BRIDGE, name, 'eth0', '192.1.2.2/16')
-            attach_container_wIP(BRIDGE, name, 'eth1', '10.1.2.2/16')
-            exec_cmd_in_container(name, '/sbin/iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE')
-        else:
-            start_container(image, name, True)            
-            attach_container(BRIDGE, name)
+
+    if args.instances == None:
+        for name,image in CONTAINER_NAME_and_IMAGE_LIST:
+            if image=='openvpn':
+                start_container(image, name, True, ' -v /users/slab/vpn:/etc/openvpn -p 1194:1194/udp')
+                attach_container_wIP(BRIDGE, name, 'eth0', '192.1.2.2/16')
+                attach_container_wIP(BRIDGE, name, 'eth1', '10.1.2.2/16')
+                exec_cmd_in_container(name, '/sbin/iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE')
+            else:
+                start_container(image, name, True)            
+                attach_container(BRIDGE, name)
         
-    connect_container_chain_wIPs(BRIDGE, NODE_0, '192.1.2.2', INGRESS_CHAIN[0:3], twoPorts=False, forward=True, includeClient=True, includeServer=False)
-    connect_container_chain_wIPs(BRIDGE, '10.1.2.2', NODE_1, INGRESS_CHAIN[3:], twoPorts=False, forward=True, includeClient=False, includeServer=True)    
-    connect_container_chain_wIPs(BRIDGE, '10.1.2.2', NODE_1, EGRESS_CHAIN[0:5], twoPorts=False, forward=False, includeClient=False, includeServer=True)
-    connect_container_chain_wIPs(BRIDGE, NODE_0, '192.1.2.2', EGRESS_CHAIN[5:], twoPorts=False, forward=False, includeClient=True, includeServer=False)    
+        connect_container_chain_wIPs(BRIDGE, NODE_0, '192.1.2.2', INGRESS_CHAIN[0:3], twoPorts=False, forward=True, includeClient=True, includeServer=False)
+        connect_container_chain_wIPs(BRIDGE, '10.1.2.2', NODE_1, INGRESS_CHAIN[3:], twoPorts=False, forward=True, includeClient=False, includeServer=True)    
+        connect_container_chain_wIPs(BRIDGE, '10.1.2.2', NODE_1, EGRESS_CHAIN[0:5], twoPorts=False, forward=False, includeClient=False, includeServer=True)
+        connect_container_chain_wIPs(BRIDGE, NODE_0, '192.1.2.2', EGRESS_CHAIN[5:], twoPorts=False, forward=False, includeClient=True, includeServer=False)
+    else:
+        name_list = []
+        client_ips = []
+        server_ips = []
+        name_list = get_names(args.instances)
+        client_ips = get_ip_range(NODE_0, args.instances)
+        printer_ips = get_ip_range(NODE_1, args.instances)
+        vpn_client_ips = get_ip_range(VPN_CLIENT_SIDE, args.instances)
+        vpn_printer_ips = get_ip_range(VPN_PRINTER_SIDE, args.instances)
+        for i in range(0, args.instances):
+            for name,image in name_list[i]:
+                ##TODO: update routing/ip address stuff
+                if image=='openvpn':
+                    start_container(image, name, True, ' -v /users/slab/vpn:/etc/openvpn -p 1194:1194/udp')
+                    attach_container_wIP(BRIDGE, name, 'eth0', str(vpn_client_ips[i])+'/16')
+                    attach_container_wIP(BRIDGE, name, 'eth1', str(vpn_printer_ips[i])+'/16')
+                    exec_cmd_in_container(name, '/sbin/iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE')
+                else:
+                    start_container(image, name, True)            
+                    attach_container(BRIDGE, name)
+            new_ingress_chain=add_chain_number(INGRESS_CHAIN, i)
+            new_egress_chain=add_chain_number(EGRESS_CHAIN, i)
+            connect_container_chain_wIPs(BRIDGE, NODE_0, vpn_client_ips[i], new_ingress_chain[0:3], twoPorts=False, forward=True, includeClient=True, includeServer=False)
+            connect_container_chain_wIPs(BRIDGE, vpn_printer_ips[i], printer_ips[i], new_ingress_chain[3:], twoPorts=False, forward=True, includeClient=False, includeServer=True)    
+            connect_container_chain_wIPs(BRIDGE, vpn_printer_ips[i], printer_ips[i], new_egress_chain[0:5], twoPorts=False, forward=False, includeClient=False, includeServer=True)
+            connect_container_chain_wIPs(BRIDGE, NODE_0, vpn_client_ips[i], new_egress_chain[5:], twoPorts=False, forward=False, includeClient=True, includeServer=False)
 
         
 if __name__ == '__main__':
