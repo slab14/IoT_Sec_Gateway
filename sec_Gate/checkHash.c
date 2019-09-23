5ecc100404d33b5ff23e651b406add00315ea634
slab <slab@dataplane.matt.psi-pg0.wisc.cloudlab.us>
2019-09-23 13:15:02 -0500

    adding gateway tools for adding and checking packet tags

---
+++ sec_Gate/checkHash.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <libnetfilter_queue/pktbuff.h>
+#include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
+#include <libnetfilter_queue/libnetfilter_queue_tcp.h>
+#include <openssl/hmac.h>
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+
+
+
+void reverseData(char *payload, int payloadLen){
+  for(unsigned int i = 0; i < payloadLen/2; ++i) {
+    char tmp = payload[i];
+    payload[i] = payload[payloadLen-1-i];
+    payload[payloadLen-1-i]=tmp;
+  }
+}
+
+char * calcHmac(char *key, struct nfq_data *tb) {
+  unsigned char *digest;
+  char *data;
+  int len;
+  len = nfq_get_payload(tb, &data);
+  digest=HMAC(EVP_md5(), key, strlen(key), (unsigned char*)data, len, NULL, NULL);
+  return digest;
+}
+
+char * newCalcHmac(char *key, uint8_t *data, uint32_t len) {
+  unsigned char *digest;
+  digest=HMAC(EVP_md5(), key, strlen(key), (unsigned char*)data, len, NULL, NULL);
+  return digest;
+}
+
+/* Callback function */
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data) {
+  unsigned char * hash;
+  char key[]="super_secret_key_for_hmac";
+  char iv[]="this is my iv";
+  //hash = calcHmac(key, nfa);
+  u_int32_t id;
+  struct nfqnl_msg_packet_hdr *ph;
+  ph=nfq_get_msg_packet_hdr(nfa);
+  id=ntohl(ph->packet_id);
+
+  //testing
+  unsigned char *rawData;
+  int len = nfq_get_payload(nfa, &rawData);
+  struct pkt_buff *pkBuff=pktb_alloc(AF_INET, rawData, len, 0x20); /* create buffer with extra space for hash value */
+  struct iphdr* ip=nfq_ip_get_hdr(pkBuff);
+  nfq_ip_set_transport_header(pkBuff, ip);
+  if(ip->protocol == IPPROTO_TCP) {
+    struct tcphdr *tcp = nfq_tcp_get_hdr(pkBuff);    
+    unsigned int payloadLen = nfq_tcp_get_payload_len(tcp, pkBuff);
+    payloadLen -= 4*tcp->th_off;
+    if(payloadLen>16){
+      /* received hash */
+      unsigned char oldHash[16];
+      char *payload = nfq_tcp_get_payload(tcp, pkBuff);      
+      memcpy(oldHash, payload, 16);
+
+      /* Remove first 16 data Bytes */
+      memmove(payload, payload+16, payloadLen-16);
+      pktb_trim(pkBuff, pktb_len(pkBuff)-16);
+      ip->tot_len=htons(pktb_len(pkBuff));
+      nfq_tcp_compute_checksum_ipv4(tcp,ip);
+      nfq_ip_set_checksum(ip);
+
+      /* Cacl hash of data */
+      hash = newCalcHmac(key, pktb_data(pkBuff), pktb_len(pkBuff));
+      
+      /*
+      for(int i=0; i<16; i++){
+	printf("%02x ", oldHash[i]);
+      }
+      printf("\n");
+      for(int i=0; i<16; i++){
+	printf("%02x ", hash[i]);
+      }
+      printf("\n-----------\n");
+      */
+      
+      if(strncmp(hash, oldHash, 16)==0) {
+	return nfq_set_verdict(qh, id, NF_ACCEPT, pktb_len(pkBuff), pktb_data(pkBuff));
+      }else {
+	return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+      }
+    }
+  }
+  return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+
+int main(int argc, char **argv) {
+  struct nfq_handle *h;
+  struct nfq_q_handle *qh;
+  int fd;
+  int rv;
+  char buf[4096] __attribute__ ((aligned));
+
+  h=nfq_open();
+  if(!h) {
+    fprintf(stderr, "error during nfq_open()\n");
+    exit(1);
+  }
+  if(nfq_unbind_pf(h, AF_INET) < 0) {
+    fprintf(stderr, "error during nfq_unfind_pf()\n");
+    exit(1);
+  }
+  if(nfq_bind_pf(h, AF_INET) < 0) {
+    fprintf(stderr, "error during nfq_bind_pf()\n");
+    exit(1);
+  }
+  /* Set callback function */
+  qh=nfq_create_queue(h,1,&cb, NULL);
+  if(!qh) {
+    fprintf(stderr, "error during nfq_create_queue()\n");
+    exit(1);
+  }
+  if(nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0){
+    fprintf(stderr, "can't set packet_copy mode\n");
+    exit(1);
+  }
+  fd=nfq_fd(h);
+  
+  while((rv=recv(fd,buf, sizeof(buf),0))) {
+    nfq_handle_packet(h, buf, rv);
+  }
+
+  nfq_destroy_queue(qh);
+  nfq_close(h);
+
+  exit(0);
+}
