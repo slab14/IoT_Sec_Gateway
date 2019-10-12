bdb0b22629c0ce942eadada85de6aa583f7eb6f1
slab <slab@dataplane.ovs2.psi-pg0.wisc.cloudlab.us>
2019-10-11 23:03:28 -0500

    initial commit of a docker container that makes a hypercall--not tested

---
+++ docker_containers/hypsign/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+gcc -O1 -o packetScript packetAppend.c -lnfnetlink -lnetfilter_queue -lpthread -lm -ldl -lssl -lcrypto
+
+gcc -O1 -o checkHash checkHash.c -lnfnetlink -lnetfilter_queue -lpthread -lm -ldl libuhcall.a
+gcc -O1 -o addHash addHash.c -lnfnetlink -lnetfilter_queue -lpthread -lm -ldl libuhcall.a
+
+while true; do
+    grep -q '^1$' "/sys/class/net/eth0/carrier" &&
+	grep -q '^1$' "/sys/class/net/eth1/carrier" &&
+	break
+
+    sleep 1
+
+done
+
+brctl addbr bridge0
+ifconfig eth0 down
+ifconfig eth1 down
+brctl addif bridge0 eth0 eth1
+ifconfig eth0 up
+ifconfig eth1 up
+ifconfig bridge0 up
+ifconfig eth0 mtu 1520
+ifconfig eth1 mtu 1520
+
+#Send packets to NFQUEUE
+iptables -t raw -A PREROUTING -i eth0 -d 10.1.1.2 -j NFQUEUE --queue-num 1
+iptables -t raw -A PREROUTING -i eth1 -d 10.1.1.2 -j NFQUEUE --queue-num 2
+
+#python -O pyscript.py
+
+#./packetScript
+
+./checkHash &
+./addHash
