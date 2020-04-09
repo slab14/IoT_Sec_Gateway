4eb530b017dde1c4d3af0af7d84ce31d496a37bb
slab <slab@node-0.newsig.psi-pg0.wisc.cloudlab.us>
2020-04-09 09:43:15 -0500

    updating SIG to work with latest SCIONlab updates

--- SIG/SIG_B_script.sh
+++ SIG/SIG_B_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-SIG_A_AS="18-ffaa:1:d12"
-SIG_A_IP="128.105.145.216"
-SIG_B_IP="128.105.145.219"
-IP="127.0.0.1"
 GO_VER="1.13.9"
+
+remote_sig_AS="18-ffaa:1:d12"
+remote_sig_IPnet="172.16.11.0/24"
+
+sigID="sigB"
+sigIP="172.16.0.12"
+LOCAL_IP="127.0.0.1"
 
 START_DIR=$(pwd)
 
@@ -24,10 +27,6 @@ export AS=$( ls /etc/scion/gen/ISD${ISD}/ | grep AS | awk -F 'AS' '{ print $2 }'
 export IAd=$(echo $AS | sed 's/_/\:/g')
 export IA=${ISD}-${AS}
 
-if ! grep -q $IAd /etc/hosts; then
-    printf "\n\n${IAd},[${SIG_B_IP}]\tlocalhost" | sudo tee -a /etc/hosts
-fi
-
 sudo systemctl start scionlab.target
 
 #Webapp
@@ -41,6 +40,7 @@ if ! grep -q GOPATH ~/.profile; then
 fi
 source ~/.profile
 mkdir -p "$GOPATH"
+
 # install golang
 if ! [ -f ~/go${GO_VER}.linux-amd64.tar.gz ]; then
     cd ~
@@ -51,17 +51,14 @@ fi
 # download scionlab's fork of scion and build and install sig
 if ! [ -d ~/scion ]; then
     cd ~
-    git clone https://github.com/netsec-ethz/scion
+    git clone -b scionlab https://github.com/netsec-ethz/scion
 fi
 
 cd ~/scion/go/sig
 go install
 
 sudo mkdir -p ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/
-if ! [ -d ~/scion ]; then
-    cd ~
-    git clone -b scionlab https://github.com/netsec-ethz/scion
-fi
+
 if ! [ -f ~/scion/go/sig/main.go ]; then
     go build -o $GOPATH/bin/sig ~/scion/go/sig/main.go
 fi
@@ -75,39 +72,41 @@ sudo sysctl net.ipv4.ip_forward=1
 cd $START_DIR
 
 #sig.conf
-if ! [ -f ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigA.config ]; then
-    sudo touch ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    cat sigB.config | sudo tee -a ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${IA}/${IA}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${IAd}/${IAd}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${AS}/${AS}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${ISD}/${ISD}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${SC}/\/etc\/scion/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
-    sudo sed -i "s/\${LOG}/${LOG}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config
+if ! [ -f ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config ]; then
+    sudo touch ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    cat sig.config | sudo tee -a ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${IA}/${IA}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${IAd}/${IAd}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${AS}/${AS}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${ISD}/${ISD}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${SC}/\/etc\/scion/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${LOG}/${LOG}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${sigID}/${sigID}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config
+    sudo sed -i "s/\${sigIP}/${sigIP}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config    
 fi
 
 #sig.json
-if ! [ -f ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigA.json ]; then
-    sudo touch ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.json
-    cat sigB.json | sudo tee -a ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.json
-    sudo sed -i "s/17-ffaa:1:XXX/$SIG_A_AS/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.json
-    sudo sed -i "s/10.0.8.XXX/$SIG_A_IP/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.json
+if ! [ -f ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.json ]; then
+    sudo touch ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.json
+    cat sig.json | sudo tee -a ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.json
+    sudo sed -i "s/\${remote_sig_AS}/${remote_sig_AS}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.json
+    sudo sed -i "s/\${remote_sig_IPnet}/${remote_sig_IPnet}/g" ${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.json
 fi
 
 #update *.topology files
 if ! grep -q sig${IAd}-1 topology.json; then
-    sed -i "s/sig17-ffaa_1_XXX"/sig${IAd}-1/g topology.json
-    sed -i "s/172.16.0.XX/${IP}/g" topology.json
+    sed -i "s/\${ISD}/${ISD}/g" topology.json
+    sed -i "s/\${IAd}/${IAd}/g" topology.json    
 fi
 
 if ! grep -q sig${IAd}-1 ${SC}/gen/ISD${ISD}/AS${AS}/endhost/topology.json; then
-    sudo sed -i '/^{/r topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/endhost/topology.json
+    sudo sed -i '/\"ISD_AS\"/e cat topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/endhost/topology.json    
 fi
 if ! grep -q sig${IAd}-1 ${SC}/gen/ISD${ISD}/AS${AS}/br${IA}-1/topology.json; then
-   sudo sed -i '/^{/r topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/br${IA}-1/topology.json
+   sudo sed -i '/\"ISD_AS\"/e cat topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/br${IA}-1/topology.json
 fi
 if ! grep -q sig${IAd}-1 ${SC}/gen/ISD${ISD}/AS${AS}/cs${IA}-1/topology.json; then
-   sudo sed -i '/^{/r topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/cs${IA}-1/topology.json
+   sudo sed -i '/\"ISD_AS\"/e cat topology.json' ${SC}/gen/ISD${ISD}/AS${AS}/cs${IA}-1/topology.json
 fi
 
 # setup
@@ -116,14 +115,14 @@ sudo modprobe dummy
 # Host B
 if ! $(ip link | grep -q dummy12); then
     sudo ip link add dummy12 type dummy
-    sudo ip addr add 172.16.0.12/32 brd + dev dummy12 label dummy12:0
-    sudo ip rule add to 172.16.11.0/24 lookup 12 prio 12
+    sudo ip addr add ${sigIP}/32 brd + dev dummy12 label dummy12:0
+    sudo ip rule add to ${remote_sig_IPnet} lookup 11 prio 11
 fi
 
 #startup SIG
 sudo mkdir -p $SC/logs/sig${IA}-1
 sudo touch $SC/logs/sig${IA}-1.log
-$GOPATH/bin/sig -config=${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/sigB.config 2>&1| sudo tee -a $SC/logs/sig${IA}-1.log >/dev/null &
+$GOPATH/bin/sig -config=${SC}/gen/ISD${ISD}/AS${AS}/sig${IA}-1/${sigID}.config &
 
 # teting
 # Host B
