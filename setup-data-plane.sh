7e358cce5df77f66ae0cc1efaa2738e835f2e62a
slab <slab@dataplane.dev.psi-pg0.wisc.cloudlab.us>
2020-05-12 23:07:40 -0500

    updating m2 file for odl

--- setup-data-plane.sh
+++ setup-data-plane.sh
@@ -94,7 +94,7 @@ setup_maven() {
     mkdir -p ~/.m2
     cp /usr/share/maven/conf/settings.xml ~/.m2/settings.xml
     cp -n ~/.m2/settings.xml{,.orig}
-    wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/stable/boron/settings.xml > ~/.m2/settings.xml
+    wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/6.0.x/settings.xml > ~/.m2/settings.xml
     export M2_HOME=/usr/share/maven/
     export M2=$M2_HOME
     export MAVEN_OPTS='-Xmx1048m -XX:MaxPermSize=512m -Xms256m'
