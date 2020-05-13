7bbb2db7c9b3ba010734bebd8a76ae7296671161
slab <slab@dataplane.dev.psi-pg0.wisc.cloudlab.us>
2020-05-13 09:02:32 -0500

    adding tool for starting ODL, manually adds missing dependency file

---
+++ ODL/startODL.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(pwd)
+if [[ $DIR == *"IoT_Sec_Gateway"* ]]; then
+    DIR=$(echo $DIR | cut -d '/' -f -3)
+fi
+if [[ $DIR == *"l2switch"* ]]; then
+    DIR=$(echo $DIR | cut -d '/' -f -3)
+fi
+# for some reason, maven is not downloading the jar for org.apache.karaf.jaas.boot:4.1.7, so we can manually download this.
+FILE="$DIR/l2switch/distribution/karaf/target/assembly/system/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar"
+# first check if we have already installed it.
+if [[ ! -f "$FILE" ]]; then
+    wget -q -O - https://repo1.maven.org/maven2/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar > $DIR/l2switch/distribution/karaf/target/assembly/system/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar
+fi
+
+# start ODL
+cd $DIR/l2switch/distribution/karaf/target/assembly
+./bin/karaf
