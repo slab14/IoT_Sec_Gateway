4a9d0ae7f411d23138adefe2a71064b362d2f4e7
brytul <60823686+brytul@users.noreply.github.com>
2020-06-09 08:48:41 -0400

    Update genTar.sh
    Creates the tar files

--- docker_containers/demo_conts/snort_base/genTar.sh
+++ docker_containers/demo_conts/snort_base/genTar.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-RULESFILE=$1
-CONFIGFILE=$2
+if [ -f "local.rules" ]; then
+        echo "Found local.rules"
+else
+        echo "Missing local.rules.  Creating one..."
+        touch local.rules
+fi
 
-tar -cf rules.tar $RULESFILE
-tar -cf conf.tar $CONFIGFILE
+echo "Tarrring rules_a"
+echo "alert icmp any any -> 10.1.1.0/24 any (msg:"ICMP Packet"; itype:8; sid:10000001; rev:1;)
+" > local.rules
+tar -cf rules_a.tar local.rules
+echo "Tarring rules_b"
+echo "drop icmp any any -> 10.1.1.0/24 any (msg:"ICMP Packet"; itype:8; sid:10000001; rev:1;)
+alert tcp any any -> 10.1.1.0/24 any (msg:"TCP Packet"; sid:10000002; rev:1;)" > local.rules
+tar -cf rules_b.tar local.rules
+
+if [ -f "snort.config" ]; then
+        echo "Found snort.conf"
+else
+        echo "Missing snort.conf.  Creating one..."
+        touch snort.conf
+fi
+
+echo "Tarring conf"
+echo "config show_year
+output alert_csv
+include rules/local.rules" > snort.conf
+tar -cf config.tar snort.conf
+
+echo "moving to dataplane's /mnt/ folder"
+mv rules_a.tar /mnt/
+mv rules_b.tar /mnt/
+mv config.tar /mnt/
