35932b69b0f2ad2616d5b941cd1f5dd56dc7df8a
slab <slab@c240g5-110219.wisc.cloudlab.us>
2020-06-09 14:36:07 -0500

    updating demo to use same image, and change functionality based upon new config files being uploaded (e.g., new snort rules)

--- docker_containers/demo_conts/snort_base/remoteKill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IP=$1
-PORT=$2
-NAME=$3
-
-curl -s -X POST http://$IP:$PORT/v1.37/containers/$NAME/kill
--- docker_containers/demo_conts/snort_base/run.sh
+++ docker_containers/demo_conts/snort_base/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-while true; do
-    grep -q '^1$' "/sys/class/net/eth1/carrier" &&
-	grep -q '^1$' "/sys/class/net/eth2/carrier" &&
-	break
+gcc -fPIC -shared -o send.so sendAlert.c -lcrypto
+gcc -O1 -o checkHash checkHash.c -lnfnetlink -lnetfilter_queue -lpthread -lm -ldl -lssl -lcrypto
+gcc -O1 -o addHash addHash.c -lnfnetlink -lnetfilter_queue -lpthread -lm -ldl -lssl -lcrypto
 
-    sleep 1
-
-done
+# setup alert path to controller
+touch ID
+echo $PROTECTION_ID > ID
+python getAlerts.py &
 
 if [[ ! -s /etc/snort/snort.conf ]]; then
     mv /etc/snort/snort.conf.default /etc/snort/snort.conf
@@ -17,9 +17,21 @@ if [[ ! -s /etc/snort/rules/local.rules ]]; then
     mv /etc/snort/rules/local.rules.default /etc/snort/rules/local.rules    
 fi
 
-# setup alert path to controller
-touch ID
-echo $PROTECTION_ID > ID
-python getAlerts.py &
+while true; do
+    grep -q '^1$' "/sys/class/net/eth1/carrier" &&
+	grep -q '^1$' "/sys/class/net/eth2/carrier" &&
+	break
 
-exec /usr/local/bin/snort "$@"
+    sleep 1
+
+done
+
+# setup packet signature actions
+iptables -t raw -A PREROUTING -i eth1 -d 10.1.1.2 -j NFQUEUE --queue-num 2
+iptables -t raw -A PREROUTING -i eth2 -d 10.1.1.2 -j NFQUEUE --queue-num 1
+./checkHash &
+./addHash &
+
+/usr/local/bin/snort $SNORT_CMD &
+
+/bin/bash
