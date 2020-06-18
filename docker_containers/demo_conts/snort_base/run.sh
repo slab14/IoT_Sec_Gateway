50081fc94a78e1fe339232142933ded245700e2d
slab <slab@c240g5-110219.wisc.cloudlab.us>
2020-06-18 10:34:21 -0500

    updates to make packet signing/hashing work inside a container

--- docker_containers/demo_conts/snort_base/run.sh
+++ docker_containers/demo_conts/snort_base/run.sh
@@ -21,14 +21,31 @@ while true; do
     grep -q '^1$' "/sys/class/net/eth1/carrier" &&
 	grep -q '^1$' "/sys/class/net/eth2/carrier" &&
 	break
-
     sleep 1
-
 done
 
+ip link add snort1 type veth peer name snort1_br
+ip link add snort2 type veth peer name snort2_br
+brctl addbr bridge1
+brctl addbr bridge2
+ip link set eth1 down
+ip link set eth2 down
+brctl addif bridge1 eth1 snort1_br
+brctl addif bridge2 eth2 snort2_br
+ip link set eth1 up mtu 1522
+ip link set eth2 up mtu 1522
+ip link set snort1 up mtu 1522
+ip link set snort1_br up mtu 1522
+ip link set snort2 up mtu 1522
+ip link set snort2_br up mtu 1522
+ip link set bridge1 up
+ip link set bridge2 up
+
 # setup packet signature actions
-iptables -t raw -A PREROUTING -i eth1 -d 10.1.1.2 -j NFQUEUE --queue-num 2
-iptables -t raw -A PREROUTING -i eth2 -d 10.1.1.2 -j NFQUEUE --queue-num 1
+#iptables -t raw -A PREROUTING -i bridge1 -j NFQUEUE --queue-num 1
+#iptables -t filter -A FORWARD -i bridge1 -j NFQUEUE --queue-num 2
+#iptables -t raw -A PREROUTING -i bridge2 -j NFQUEUE --queue-num 1
+#iptables -t filter -A FORWARD -i bridge2 -j NFQUEUE --queue-num 2
 ./checkHash &
 ./addHash &
 
