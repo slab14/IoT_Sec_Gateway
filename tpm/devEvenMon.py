cf31c0982b57d832269e989ae196bf4e62795753
slab <slab@c240g5-110219.wisc.cloudlab.us>
2020-06-18 20:54:30 -0500

    developing a tool for measuring code on docker container

---
+++ tpm/devEvenMon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+## notes. requires python3, & docker-py pip module
+
+import json
+import subprocess
+import six
+from time import sleep
+from docker import Client
+from binascii import hexlify, unhexlify
+
+def get_status(event):
+    ret=''
+    if 'status' in event:
+        ret=event['status']
+    return ret
+
+def getNewContID(event):
+    ID=event['id']
+    return ID
+
+def extendPCR(hashData, register='16'):
+    if isinstance(hashData, six.string_types):
+        subprocess.check_call(["echo", register+':sha256='+hashData])
+        
+def sha1Contbin(cli, contID):
+    sleep(1)
+    execID=cli.exec_create(container=contID, cmd=["find", "bin", "-type", "f", "-exec", "sha1sum", "\{\}", "\\;", "|", "sha1sum"])['Id']
+
+    outHash=cli.exec_start(execID)
+    print(outHash)
+
+        
+def main():
+    cli = Client(base_url='unix://var/run/docker.sock')
+    for event in cli.events(decode=True):
+        eventStatus=get_status(event)
+        if eventStatus in ['create', 'die']:
+            contID=getNewContID(event)
+            sha1Contbin(cli, contID)
+        
+
+if __name__=='__main__':
+    main()
