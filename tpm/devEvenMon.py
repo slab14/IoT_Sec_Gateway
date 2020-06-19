195d3de3d883d61c84f92eeddd8f0a72ef79a4f7
slab <slab@dataplane.new1.psi-pg0.wisc.cloudlab.us>
2020-06-19 12:39:17 -0500

    updating to support checking container image sha1 hashes

--- tpm/devEvenMon.py
+++ tpm/devEvenMon.py
@@ -5,9 +5,15 @@
 import json
 import subprocess
 import six
+import socket
+import ctypes
 from time import sleep
 from docker import Client
 from binascii import hexlify, unhexlify
+
+sender = ctypes.CDLL("/users/slab/IoT_Sec_Gateway/tpm/send.so")
+sender.sendEncrypted.argtype=[ctypes.POINTER(ctypes.c_char), ctypes.c_int]
+sender.sendEncrypted.restype=ctypes.c_int
 
 def get_status(event):
     ret=''
@@ -25,19 +31,34 @@ def extendPCR(hashData, register='16'):
         
 def sha1Contbin(cli, contID):
     sleep(1)
+    execID=cli.exec_create(container=contID, cmd=["cat", "ID"])
+    execOut=cli.exec_start(execID)
+    devID=execOut.split(b'\n')[0].decode("utf-8") 
     execID=cli.exec_create(container=contID, cmd="sh -c 'find sbin bin usr -type f -exec sha1sum {} \; | sha1sum'")
     outHash=cli.exec_start(execID)
-    ret = outHash.split(b'  -')[0]
-    print(ret)
+    imgHash = outHash.split(b'  -')[0].decode("utf-8") 
+    return(devID, imgHash)
 
+
+def sendData(msg):
+    cipherLen=sender.sendEncrypted(msg, len(msg))
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('128.105.146.14', 38687))
+    s.sendall(bytes(msg, 'utf-8'))
+    s.close()
+    '''
         
 def main():
     cli = Client(base_url='unix://var/run/docker.sock')
     for event in cli.events(decode=True):
         eventStatus=get_status(event)
-        if eventStatus in ['create', 'die']:
+        if eventStatus in ['start']:
             contID=getNewContID(event)
-            sha1Contbin(cli, contID)
+            (devID,imgHash)=sha1Contbin(cli, contID)
+            msg="ID: {};SHA1: {}".format(devID, imgHash)
+            print(msg)
+            sendData(bytes(msg, 'utf-8'))
         
 
 if __name__=='__main__':
