dbcc2476c62a170e497609a9ced5acde7638887e
slab <slab@c240g5-110227.wisc.cloudlab.us>
2020-08-06 13:15:00 -0500

    updating for working conn limit patch

--- docker_containers/demo_conts/iptables_demoa/run.sh
+++ docker_containers/demo_conts/iptables_demoa/run.sh
@@ -9,6 +9,7 @@ while true; do
     sleep 1
 done
 
+#setup bridge
 brctl addbr bridge0
 ifconfig eth1 down
 ifconfig eth2 down
@@ -17,21 +18,21 @@ ifconfig eth1 up
 ifconfig eth2 up
 ifconfig bridge0 up
 
-
-
+#maps to policy
 touch ID
 echo $PROTECTION_ID > ID
-service ulogd2 start
-python getAlerts.py
-
-setup_iptables.sh
 
 #iptables -A FORWARD -p icmp -s 10.1.1.0/24 -j NFLOG --nflog-prefix "iptables: " --nflog-group 1
 
-#Limit number of simultaneous connections: --connlimit-above X, currently 15--allows 14 connections, drops the 15th
-#iptables -A FORWARD -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m connlimit --connlimit-above $MAX_CONN --connlimit-mask $CONN_MASK --connlimit-saddr -j REJECT --reject-with tcp-reset
-#Limit the throughput (in either pkts/sec or Bytes/sec, for granularity/group desired--src&dst, only dst, dst&dstport, etc; specified in --hashlimit-mode). --hashlimit-above X/time (or Xb/time for Bytes, does not allow modifiers such as K or M)
-#iptables -A FORWARD -m hashlimit --hashlimit-above "$MAX_RATE"/sec --hashlimit-mode $MODE --hashlimit-name foo -j DROP
+# run iptables rules from controller
+while ![ -f setup_iptables.sh]; do
+    sleep 1
+done
+chmod +x setup_iptables.sh
+./setup_iptables.sh
 
+
+service ulogd2 start
+python getAlerts.py &
 
 /bin/bash
