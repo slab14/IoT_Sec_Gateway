0d77c6e38cf881ebe21989b019fbb88edad04aec
slab14 <mmccorm1@andrew.cmu.edu>
2020-08-19 22:48:29 -0700

    updates to rule generation to automake FSM to snort flow-bit based rules, currently not fully operational--getting error on loop in same state

--- radio/newModel2Rule.py
+++ radio/newModel2Rule.py
@@ -10,11 +10,11 @@ class FSM():
         self.transMatrix = transMatrix
         self.initial = initial
         self.proto = {}
-        self.out = ''
-        
+        self.out = {}
+        self.inLen = {}
+        self.outLen = {}
         # need for snort rule IDs
         self.rule_id = 1000000
-        
         #check if initial state is in states
         if self.initial not in self.states:
             print("WARNING: Initial state not found.")
@@ -30,6 +30,7 @@ class FSM():
         self.readContent()
         self.SERVER_PORT=''
         self.GROUPNAME=''
+        self.tranState=0
         
     def __repr__(self):
         v = "\n".join(str(r) for r in self.transMatrix)
@@ -40,25 +41,25 @@ Transitions:
 Initial state: {self.initial}
 '''
 
+    def getNewRuleID(self):
+        out = self.rule_id
+        self.rule_id+=1
+        return out
+    
+    def resetTranState(self):
+        self.tranState=0
+
+    def getAndIncTranState(self):
+        out = self.tranState
+        self.tranState+=1
+        return out
 
     def setGroupName(self, name):
         self.GROUPNAME=name
+
+    def getIndex(self, sid, tid):
+        return self.transMatrix[sid][tid]
     
-    def generateFlowbitsOptions(self, sid, tid):
-        # Generate the flowbits rule to check if in state S
-        if sid == self.init_id:
-            s_bits = f'isnotset,any,{self.GROUPNAME}'
-        else:
-            s_bits = f'isset,{self.states[sid]}'
-        
-        # Generate the flowbits rule to set state T
-        if tid == self.init_id:
-            t_bits = f'unset,all,{self.GROUPNAME}'
-        else:
-            t_bits = f'setx,{self.states[tid]},{self.GROUPNAME}'
-        
-        return f'flowbits:{s_bits};flowbits:{t_bits};'
-        
     def readContent(self):
         """This function reads the protocol specifications and fill self.proto"""
         with open(self.protofile, "r") as f:
@@ -77,21 +78,67 @@ Initial state: {self.initial}
                         inP = False
                     continue
                 line = line.rstrip('\n')
+                contents = line.split(' - ')
+                if (len(contents) < 3):
+                    print("WARNING: Protocol specification in the wrong format.")
+                    break
                 if inP:
-                    contents = line.split(' - ')
-                    if (len(contents) < 2):
-                        print("WARNING: Protocol specification in the wrong format.")
-                        break
                     self.proto[contents[0]] = contents[1]
+                    self.inLen[contents[0]] = int(contents[2])
                 elif outP:
-                    self.out = line
+                    self.out[contents[0]] = contents[1]
+                    lenRange=contents[2].split(',')
+                    if (int(lenRange[0]) == int(lenRange[1])):
+                        self.outLen[contents[0]] = [int(lenRange[0])]
+                    else:
+                        self.outLen[contents[0]] = [int(lenRange[0]),int(lenRange[1])]
 
+    def generateStateFlowbitsOptions(self, sid, tid, transit=False, final=False):
+        #initial transition from state S -> T
+        out=''
+        if ((not transit) and (not final)):
+            # Generate the flowbits rule to check if in state S
+            if sid == self.init_id:
+                s_bits = f'isnotset,any,{self.GROUPNAME}'
+            else:
+                s_bits = f'isset,{self.states[sid]}'
+            # Generate the flowbits rule to set state T
+            if tid == self.init_id:
+                t_bits = f'unset,all,{self.GROUPNAME}'
+            else:
+                t_bits = f'setx,{self.states[tid]}.{self.states[sid]},{self.GROUPNAME}'
+            out = f'flowbits:{s_bits};flowbits:{t_bits};'
+        # Part-way through transition from S->T
+        elif transit:
+            s_bits = f'isset,{self.states[tid]}.{self.states[sid]}'
+            out = f'flowbits:{s_bits};'
+        # Final step before transition is complete to T
+        elif final:
+            s_bits = f'isset,{self.states[tid]}.{self.states[sid]}'
+            t_bits = f'setx,{self.states[tid]},{self.GROUPNAME}'
+            out = f'flowbits:{s_bits};flowbits:{t_bits};'
+        return out
+
+    def generateTransitFlowbits(self, init=False, mult=False):
+        out=''
+        if init:
+            self.resetTranState()
+            out = f'flowbits:setx,t{self.getAndIncTranState()},delta;'
+        else:
+            state = self.getAndIncTranState()
+            priorState=state-1
+            out = f'flowbits:isset,t{priorState};'
+            if mult:
+                out += f'flowbits:set,t{state},delta;'
+            else:
+                out+= f'flowbits:setx,t{state},delta;'
+        return out
+    
     def generateContent(self, sid, tid): 
-        content = ''
-        for entry in self.proto:
-            if entry in self.transMatrix[sid][tid]:
-                content += self.proto[entry]
-        return content
+        return self.proto[self.getIndex(sid,tid)]
+
+    def generateResponseContent(self, sid, tid): 
+        return self.out[self.getIndex(sid,tid)]    
 
     def generateHeader(self, read, tcp=False):
         server = f'any {self.SERVER_PORT}'
@@ -105,27 +152,54 @@ Initial state: {self.initial}
             return f'pass {msgType} {server} -> {client}'
 
     def generateRule(self, sid, tid, input=True):
+        rule_id = f'sid:{self.getNewRuleID()};'
+        addPkt=False
+        if (self.inLen[self.getIndex(sid,tid)]!=0):
+            addPkt=True
+        # Initial rule for starting state transitions (request)
         content = self.generateContent(sid, tid)
-        flowbits = self.generateFlowbitsOptions(sid, tid)
-        rule_id = f'sid:{self.rule_id};'
-        
-        self.rule_id += 1
-
+        stateFlowbits = self.generateStateFlowbitsOptions(sid, tid)
+        transFlowbits = self.generateTransitFlowbits(True)
         header = self.generateHeader(True)
-        return f'{header} (flow:established;{content}{flowbits}tag:session,exclusive;{rule_id})' 
-        '''
-        #Assume transitions that contains read is query from client to server, transition that contains
-        #response is from server to client
-        if "Query" in self.transMatrix[sid][tid]:
-            header = self.generateHeader(True)
-            return f'{header} (flow:established;{content}{flowbits}tag:session,exclusive;{rule_id})' 
-        elif "Response" in self.transMatrix[sid][tid]:
-            header = self.generateHeader(False)
-            return f'{header} (flow:established;{content}{flowbits}tag:session,exclusive;{rule_id})' 
+        rules = f'{header} ({content}{stateFlowbits}{transFlowbits}{rule_id})\n'
+        # check if additional packets are expected in request
+        if (addPkt):
+            if (self.inLen[self.getIndex(sid,tid)]>1460):
+                size = f'dsize:>0;'
+                transFlowbits = self.generateTransitFlowbits(mult=True)
+            else:
+                size = f'dsize:{self.inLen[self.getIndex(sid,tid)]};'
+                transFlowbits = self.generateTransitFlowbits()
+            rule_id = f'sid:{self.getNewRuleID()};'                
+            stateFlowbits = self.generateStateFlowbitsOptions(sid, tid, transit=True)
+            rules += f'{header} ({size}{stateFlowbits}{transFlowbits}{rule_id})\n'
+        # Handle reply
+        content = self.generateResponseContent(sid, tid)
+        if (addPkt):        
+            stateFlowbits = self.generateStateFlowbitsOptions(sid, tid, transit=True)
         else:
-            print("WARNING: Protocal specification in wrong format, cannot decide direction of flow.")
-            return ''
-        '''
+            stateFlowbits = self.generateStateFlowbitsOptions(sid, tid, final=True)            
+        transFlowbits = self.generateTransitFlowbits()
+        header = self.generateHeader(False)
+        rule_id = f'sid:{self.getNewRuleID()};'        
+        rules += f'{header} ({content}{stateFlowbits}{transFlowbits}{rule_id})\n'
+        # additional reply packets
+        if (addPkt):
+            size=''
+            limit=self.outLen[self.getIndex(sid,tid)]
+            if len(limit)>1:
+                limit[0]=int(round(limit[0]*.8))
+                limit[1]=int(round(limit[1]*1.2))                
+                size = f'dsize:{limit[0]}<>{limit[1]};'
+            else:
+                size = f'dsize:{limit[0]};'
+            stateFlowbits = self.generateStateFlowbitsOptions(sid, tid, final=True)            
+            transFlowbits = self.generateTransitFlowbits()
+            header = self.generateHeader(False)
+            rule_id = f'sid:{self.getNewRuleID()};'            
+            rules += f'{header} ({size}{stateFlowbits}{transFlowbits}{rule_id})'
+        return rules
+
     
     def allowSYN(self):
         #allow client to send SYN packets to server
@@ -188,13 +262,15 @@ Initial state: {self.initial}
             print(self.allowSYN())
             print(self.allowSYNACK())
             print(self.allowACKs())
-            print(self.addOut())
+            print(self.allowFIN())            
+            #print(self.addOut())
             print(self.dropAll())
         else:
             rules.append(self.allowSYN())
             rules.append(self.allowSYNACK())
             rules.append(self.allowACKs())
-            rules.append(self.addOut())
+            rules.append(self.allowFIN())            
+            #rules.append(self.addOut())
             rules.append(self.dropAll())
         with open(outfile, 'w') as f:
             for rule in rules:
